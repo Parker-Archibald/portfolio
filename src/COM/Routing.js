772ffb2031493980ece8aeaa3642608f494c6492
Parker <parkerarchibald97@gmail.com>
2022-09-27 10:14:44 -0600

    test

--- src/COM/Routing.js
+++ src/COM/Routing.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Home from '../Components/Home';
-import SpotifyApp from '../Pages/SpotifyApp';
+// import SpotifyApp from '../Pages/SpotifyApp';
 import Solitude from '../Pages/Solitude';
 
 const Routing = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />}/>
-            <Route path='/spotify' element={<SpotifyApp/>}/>
+            {/* <Route path='/spotify' element={<SpotifyApp/>}/> */}
             <Route path='/solitude' element={<Solitude/>}/>
         </Routes>
     )
